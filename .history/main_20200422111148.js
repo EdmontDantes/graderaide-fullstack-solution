7aebcedb3c1dd96c33e0c42dd260dc3165caed15
Bogdan <bogdan@En760.kowaltchook.com>
2020-04-23 09:17:16 -0400

    as far as could get

---
+++ .history/main_20200422111148.js
@@ -0,0 +1,65 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+
+const Grade = (assignment, score) => {
+    return {
+      assignment,
+      score,
+    }
+  }
+  
+  const Term = (hours) => {
+    return {
+      hours,
+      grades: [],
+  
+      addGrade: function(assignment, score) {
+        const grade = Grade(assignment, score);
+        this.grades.push(grade);
+      },
+    }
+  }
+  
+  const Course = (name) => {
+    return {
+      name,
+      terms: [],
+  
+      addTerm: function(hours) {
+        const term = Term(hours);
+        this.terms.push(term);
+      }
+    }
+  }
+  
+  const Student = (name, course, term = 1) => {
+    return {
+      name,
+      course,
+      term,
+      courses: [],
+  
+      addCourse: function(name) {
+        this.courses.push(Course(name));
+      },
+  
+      getAverage: function(courseToFind = this.course, termIndex = this.term - 1) {
+        let foundCourse;
+        for (const course of this.courses) {
+          if (course.name === courseToFind) {
+            foundCourse = course;
+          }
+        }
+  
+        let sum = 0;
+        const grades = foundCourse.terms[termIndex].grades;
+        for (const grade of grades) {
+          sum += grade.score;
+        }
+  
+        return sum / grades.length;
+      },
+    }
+  }
+  
---
+++ .history/main_20200422124700.js
@@ -0,0 +1,101 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+
+const Grade = (assignment, score) => {
+    return {
+        assignment,
+        score,
+    }
+}
+
+const Term = () => {
+    return {
+    grades: [],
+
+    addGrade: function(assignment, score) {
+        const grade = Grade(assignment, score);
+        this.grades.push(grade);
+    },
+    }
+}
+
+const Course = (name) => {
+    return {
+    name,
+    terms: [Term()],
+
+    addTerm: function() {
+        const term = Term();
+        this.terms.push(term);
+    }
+    }
+}
+
+const Student = (name, course, term = 1) => {
+    return {
+    name,
+    course,
+    term,
+    courses: [Course(course)],
+
+    addCourse: function(course) {
+        this.courses.push(Course(course));
+        this.course = course;
+    },
+
+    getAverage: function(courseToFind = this.course, termIndex = this.term - 1) {
+        let foundCourse;
+        for (const course of this.courses) {
+        if (course.name === courseToFind) {
+            foundCourse = course;
+        }
+        }
+
+        const grades = foundCourse.terms[termIndex].grades;
+        if (grades.length === 0) {
+        return 'No grades yet.'
+        }
+
+        let sum = 0;
+        for (const grade of grades) {
+        sum += grade.score;
+        }
+
+        return sum / grades.length;
+    },
+    }
+}
+
+const createNewStudentTileQuery = document.querySelector('#create-new-student-tile');
+const newStudentNameQuery = document.querySelector('.new-student-name');
+const newStudentCourseNameQuery = document.querySelector('.new-student-course');
+const newStudentTermNumOptionQuery = document.querySelector('.new-student-term');
+const btnNewStudentQuery = document.querySelector('.new-student-button');
+
+const newStudentTile = () => {
+    let name = '';
+    let course = '';
+    let termNum = 0;
+    Student
+    name = newStudentNameQuery.value;
+    course = newStudentCourseNameQuery.value;
+    termNum = newStudentTermNumOptionQuery.value;
+
+    // for (const newStudentId of )
+
+    const studentInfo = document.createElement('ul');
+    const nameItem = document.createElement('li');
+    const courseItem = document.createElement('li');
+    const termItem  = document.createElement('li');
+    
+    nameItem.innerText = `Name: ${student.name}`;
+    courseItem.innerText = `Course: ${student.course}`;
+    termItem.innerText = `Term: ${student.term}`;
+    
+    studentInfo.classList.add('student-info');
+    studentInfo.appendChild(nameItem);
+    studentInfo.appendChild(courseItem);
+    studentInfo.appendChild(termItem);
+    someParentElement.appendChild(studentInfo);
+}
